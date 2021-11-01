43bb8fdc4fdf3a4ad3322d62fe4c79d2b280c690
callummole <cmole@turing.ac.uk>
2021-11-01 13:54:34 +0000

    update for pySim v1.0

--- QR_based_applet/host/setup_fs.py
+++ QR_based_applet/host/setup_fs.py
@@ -5,8 +5,8 @@
 # 1. Writes DF_GRAPHICS (5f50), EF_IMG (4f20) and EF_INSTANCE (4f01) to a Sysmocom SJ-A2
 # 2. Inserts EF_INSTANCE entry into EF.IMG
 from pySim.transport.pcsc import PcscSimLink
-from pySim.card_handler import card_handler
-from pySim.cards import card_detect, Card
+from pySim.card_handler import Cardhandler
+from pySim.cards import card_detect, SimCard
 from pySim.commands import SimCardCommands
 from pySim.utils import h2b, sanitize_pin_adm
 from pySim.filesystem import CardMF, RuntimeState, CardDF, CardADF
@@ -118,7 +118,7 @@ def main():
 
 	sl.wait_for_card()
 
-	cardhandler = card_handler(sl)
+	cardhandler = CardHandler(sl)
 
 	card = card_detect("auto", scc)
 	if card is None:
